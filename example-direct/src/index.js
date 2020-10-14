715660dafdb3ba8b310f8e6765fae55c00f12d47
Matteo Manzinello <matteo@manzinello.com>
2020-10-14 22:44:28 +0200

    new webpack direct

--- example-direct/src/index.js
+++ example-direct/src/index.js
@@ -1,3 +1,18 @@
-import mailgo from "../../../mailgo/lib/mailgo";
+import mailgo, {
+  mailgoDirectRender,
+  isMailgo,
+} from "../../../mailgo/lib/mailgo";
+
+// mailgoDirectRender("mailto:matteo@manzinello.dev");
 
 mailgo();
+
+document.getElementById("direct-button").addEventListener("click", () => {
+  mailgoDirectRender("mailto:matteo@manzinello.dev");
+});
+
+document.getElementById("direct-button-tel").addEventListener("click", () => {
+  mailgoDirectRender("tel:123456789");
+});
+
+// console.log(isMailgo);
